f8ccd8c8aeaa5dde6bf37758059da6cf2fb471ca
Sam Dods <sam@theappbusiness.com>
2013-12-30 20:14:53 +0000

    Initial Commit

---
+++ DynamicCategoryProperties/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DynamicCategoryProperties
+//
+//  Created by Sam Dods on 30/12/2013.
+//  Copyright (c) 2013 Sam Dods. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DynamicCategoryProperties/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  DynamicCategoryProperties
+//
+//  Created by Sam Dods on 30/12/2013.
+//  Copyright (c) 2013 Sam Dods. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
